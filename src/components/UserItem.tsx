23d37914db2c8743f442c1f7aea5c607f2ed7f6e
Masud Rana <masud.cphd@gmail.com>
2025-04-29 00:53:52 +0600

    name

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -3,7 +3,7 @@
 export default function UserItem() {
     return <div className="flex items-center justify-between gap-2 border rounded-[8px]  p-2">
         <div className="avatar min-h-10 min-w-10 rounded-full bg-emerald-700 text-white flex items-center justify-center">
-            <p>MR</p>
+            <p>MRB</p>
         </div>
         <div className="grow">
             <p className="text-[16px] font-bold ">Masud Rana</p>
